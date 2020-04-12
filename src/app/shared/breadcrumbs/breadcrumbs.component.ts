fa409fc5f1e63fa020eb9c781c97da83addec5a9
edwar <edwar_kenedy@hotmail.com>
2020-04-12 15:45:46 -0400

    Sección 8 terminada

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styles: []
+  styles: [],
 })
 export class BreadcrumbsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  titulo: string;
+  constructor(
+    private router: Router,
+    private title: Title,
+    private meta: Meta
+  ) {
+    this.getDataRoute().subscribe((data) => {
+      console.log(data);
+      this.titulo = data.titulo;
+      this.title.setTitle(this.titulo);
+      const metaTag: MetaDefinition = {
+        name: 'description',
+        content: data.titulo,
+      };
+      this.meta.updateTag(metaTag);
+    });
   }
 
+  ngOnInit(): void {}
+
+  getDataRoute(): Observable<{ titulo: string }> {
+    return this.router.events.pipe(
+      filter((evento) => evento instanceof ActivationEnd),
+      filter((evento: ActivationEnd) => evento.snapshot.firstChild === null),
+      map((evento: ActivationEnd) => evento.snapshot.data as { titulo: string })
+    );
+  }
 }
